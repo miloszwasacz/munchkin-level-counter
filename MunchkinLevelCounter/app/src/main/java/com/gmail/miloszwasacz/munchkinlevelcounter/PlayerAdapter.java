625b1b000cea3c11d8d0ce348ec21d6e0d2aa7da
miloszwasacz <48798388+miloszwasacz@users.noreply.github.com>
2019-11-28 21:07:55 +0100

    Migrate to AndroidX

--- MunchkinLevelCounter/app/src/main/java/com/gmail/miloszwasacz/munchkinlevelcounter/PlayerAdapter.java
+++ MunchkinLevelCounter/app/src/main/java/com/gmail/miloszwasacz/munchkinlevelcounter/PlayerAdapter.java
@@ -1,7 +1,6 @@
 package com.gmail.miloszwasacz.munchkinlevelcounter;
 
-import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
